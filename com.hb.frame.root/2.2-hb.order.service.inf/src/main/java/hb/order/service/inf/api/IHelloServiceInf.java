6104d5485daffc03c9475a24a030688cf8368f7b
yzd <tedijoteamo@163.com>
2018-07-22 00:13:56 +0800

    //SERVICE_INF_PATH=接口名+"/";     final String SERVICE_INF_PATH="IHelloServiceInf"+"/";

--- com.hb.frame.root/2.2-hb.order.service.inf/src/main/java/hb/order/service/inf/api/IHelloServiceInf.java
+++ com.hb.frame.root/2.2-hb.order.service.inf/src/main/java/hb/order/service/inf/api/IHelloServiceInf.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public interface IHelloServiceInf {
     //解决：不要在接口类名上使用RequestMapping，虽然可以使用，
     //但同时SpringMVC会把该接口的实例当作Controller开放出去，这个可以在启动的Mapping日志中查看到
-    final String SERVICE_INF_PATH="IHelloServiceInf/";
+    //SERVICE_INF_PATH=接口名+"/";
+    final String SERVICE_INF_PATH="IHelloServiceInf"+"/";
     @RequestMapping(value = SERVICE_INF_PATH+"world", method = RequestMethod.POST)
     WorldBO world(@RequestBody WorldDTO user);
 }
